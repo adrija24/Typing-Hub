ca2afe159115b0ed6ddee4b5fb6f1a8dd65b8895
adrija24 <24adrijaroy@gmail.com>
2023-07-24 09:18:29 +0530

    Complete Astro typing test project added as a new repo

---
+++ src/assets/js/levelData.js
@@ -0,0 +1,303 @@
+const levelData = [
+    {
+        level: 1,
+        speed: 20,
+        finalAccuracy: 90,
+    },
+
+    {
+        level: 2,
+        speed: 22,
+        finalAccuracy: 90, 
+    },
+
+    {
+        level: 3,
+        speed: 24,
+        finalAccuracy: 91,
+    },
+
+    {
+        level: 4,
+        speed: 26,
+        finalAccuracy: 91,
+    },
+
+    {
+        level: 5,
+        speed: 28,
+        finalAccuracy: 92,
+    },
+
+    {
+        level: 6,
+        speed: 30,
+        finalAccuracy: 92,
+    },
+
+    {
+        level: 7,
+        speed: 32,
+        finalAccuracy: 93,
+    },
+
+    {
+        level: 8,
+        speed: 34,
+        finalAccuracy: 93,
+    },
+
+    {
+        level: 9,
+        speed: 36,
+        finalAccuracy: 94,
+    },
+
+    {
+        level: 10,
+        speed: 38,
+        finalAccuracy: 94,
+    },
+
+    {
+        level: 11,
+        speed: 40,
+        finalAccuracy: 95,
+    },
+
+    {
+        level: 12,
+        speed: 42,
+        finalAccuracy: 95,
+    },
+
+    {
+        level: 13,
+        speed: 44,
+        finalAccuracy: 95,
+    },
+
+    {
+        level: 14,
+        speed: 46,
+        finalAccuracy: 95,
+    },
+
+    {
+        level: 15,
+        speed: 48,
+        finalAccuracy: 96,
+    },
+
+    {
+        level: 16,
+        speed: 50,
+        finalAccuracy: 97,
+    },
+
+    {
+        level: 17,
+        speed: 52,
+        finalAccuracy: 98,
+    },
+
+    {
+        level: 18,
+        speed: 54,
+        finalAccuracy: 99,
+    },
+
+    {
+        level: 19,
+        speed: 56,
+        finalAccuracy: 100,
+    },
+
+    {
+        level: 20,
+        speed: 58,
+        finalAccuracy: 100,
+    },
+
+    {
+    level: 21,
+    speed: 60,
+    finalAccuracy: 100,
+},
+
+{
+    level: 22,
+    speed: 62,
+    finalAccuracy: 100,
+},
+
+{
+    level: 23,
+    speed: 64,
+    finalAccuracy: 100,
+},
+
+{
+    level: 24,
+    speed: 66,
+    finalAccuracy: 100,
+},
+
+{
+    level: 25,
+    speed: 68,
+    finalAccuracy: 100,
+},
+
+{
+    level: 26,
+    speed: 70,
+    finalAccuracy: 100,
+},
+
+{
+    level: 27,
+    speed: 72,
+    finalAccuracy: 100,
+},
+
+{
+    level: 28,
+    speed: 74,
+    finalAccuracy: 100,
+},
+
+{
+    level: 29,
+    speed: 76,
+    finalAccuracy: 100,
+},
+
+{
+    level: 30,
+    speed: 78,
+    finalAccuracy: 100,
+},
+
+{
+    level: 31,
+    speed: 80,
+    finalAccuracy: 100,
+},
+
+{
+    level: 32,
+    speed: 82,
+    finalAccuracy: 100,
+},
+
+{
+    level: 33,
+    speed: 83,
+    finalAccuracy: 100,
+},
+
+{
+    level: 34,
+    speed: 84,
+    finalAccuracy: 100,
+},
+
+{
+    level: 35,
+    speed: 85,
+    finalAccuracy: 100,
+},
+
+{
+    level: 36,
+    speed: 86,
+    finalAccuracy: 100,
+},
+
+{
+    level: 37,
+    speed: 87,
+    finalAccuracy: 100,
+},
+
+{
+    level: 38,
+    speed: 88,
+    finalAccuracy: 100,
+},
+
+{
+    level: 39,
+    speed: 89,
+    finalAccuracy: 100,
+},
+
+{
+    level: 40,
+    speed: 90,
+    finalAccuracy: 100,
+},
+
+{
+    level: 41,
+    speed: 91,
+    finalAccuracy: 100,
+},
+
+{
+    level: 42,
+    speed: 92,
+    finalAccuracy: 100,
+},
+
+{
+    level: 43,
+    speed: 93,
+    finalAccuracy: 100,
+},
+
+{
+    level: 44,
+    speed: 94,
+    finalAccuracy: 100,
+},
+
+{
+    level: 45,
+    speed: 95,
+    finalAccuracy: 100,
+},
+
+{
+    level: 46,
+    speed: 96,
+    finalAccuracy: 100,
+},
+
+{
+    level: 47,
+    speed: 97,
+    finalAccuracy: 100,
+},
+
+{
+    level: 48,
+    speed: 98,
+    finalAccuracy: 100,
+},
+
+{
+    level: 49,
+    speed: 99,
+    finalAccuracy: 100,
+},
+
+{
+    level: 50,
+    speed: 100,
+    finalAccuracy: 100,
+},
+
+];
+export default levelData
